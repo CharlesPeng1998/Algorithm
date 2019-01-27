e2c844835504df17d4a762bae8e7f10ce3e6a41b
CharlesPeng <charlespeng1998@outlook.com>
2019-01-27 21:42:30 +0800

    2019_1_27

--- POJ - 3009.cpp
+++ POJ - 3009.cpp
@@ -1,233 +1,58 @@
 //这道题有必要写入博客
+//此为AC代码
 #include <iostream>
 #include <string.h>
+#define INF 0x3f3f3f3f
+#define judge(i,j) i>0&&i<=h&&j>0&&j<=w&&board[i][j]!=1
 using namespace std;
 
-int w,h;
 int board[21][21];
-int visited[21][21];
-int res;
+int w,h;
+int si,sj,gi,gj;
+int res,steps;
+int di[]={1,-1,0,0};
+int dj[]={0,0,1,-1};
 
-int dfs_up(int i,int j,int n);
-int dfs_down(int i,int j,int n);
-int dfs_left(int i,int j,int n);
-int dfs_right(int i,int j,int n);
-
-int dfs_up(int i,int j,int n) {
-  if (i<=0 || i>h || j<=0 || j>w) return 0; //出界
-  if (board[i][j]==1) { //撞墙
-    board[i][j]=0;
-    return -1;
-  }
-  if (board[i][j]==3) {
-    if (n<res || res==0) res=n;
-    return 1; //到达终点
-  }
-  //继续向上移动
-  int stat=dfs_up(i-1,j,n);
-  if (stat==1) return 1;
-  if (stat==0) return 0;
-  if (stat==-1) {
-    if (i-1>0 && board[i-1][j]!=1) {
-      if (dfs_up(i-1,j,n+1)) {
-        board[i-1][j]=1;
-        return 1;
+void dfs(int i,int j) {
+  if (steps>10) return;
+   //判断当前位置周围是否存在可移动方向的标志
+  for (int k=0;k<4;k++) {
+    int ok=0;
+    int pi=i+di[k]; int pj=j+dj[k];
+    while (judge(pi,pj)) {
+      ok=1; //周围存在可移动方向
+      if (board[pi][pj]==3) {
+        if (steps<res) res=steps;
       }
+      pi+=di[k]; pj+=dj[k];
     }
-    if (i+1<=h && board[i+1][j]!=1) {
-      if (dfs_down(i+1,j,n+1)) {
-        board[i-1][j]=1;
-        return 1;
-      }
-    }
-    if (j-1>0 && board[i][j-1]!=1) {
-      if (dfs_left(i,j-1,n+1)) {
-        board[i-1][j]=1;
-        return 1;
-      }
-    }
-    if (j+1<=w && board[i][j+1]!=1) {
-      if (dfs_right(i,j+1,n+1)) {
-        board[i-1][j]=1;
-        return 1;
-      }
+    if (board[pi][pj]==1&&ok) {
+      board[pi][pj]=0;
+      steps++;
+      dfs(pi-di[k],pj-dj[k]);
+      steps--;
+      board[pi][pj]=1;
     }
   }
-
-  return 0;
 }
-int dfs_down(int i,int j,int n) {
-  if (i<=0 || i>h || j<=0 || j>w) return 0; //出界
-  if (board[i][j]==1) { //撞墙
-    board[i][j]=0;
-    return -1;
-  }
-  if (board[i][j]==3) {
-    if (n<res || res==0) res=n;
-    return 1; //到达终点
-  }
-  //继续向上移动
-  int stat=dfs_down(i+1,j,n);
-  if (stat==1) return 1;
-  if (stat==0) return 0;
-  if (stat==-1) {
-    if (i-1>0 && board[i-1][j]!=1) {
-      if (dfs_up(i-1,j,n+1)) {
-        board[i+1][j]=1;
-        return 1;
-      }
-    }
-    if (i+1<=h && board[i+1][j]!=1) {
-      if (dfs_down(i+1,j,n+1)) {
-        board[i+1][j]=1;
-        return 1;
-      }
-    }
-    if (j-1>0 && board[i][j-1]!=1) {
-      if (dfs_left(i,j-1,n+1)) {
-        board[i+1][j]=1;
-        return 1;
-      }
-    }
-    if (j+1<=w && board[i][j+1]!=1) {
-      if (dfs_right(i,j+1,n+1)) {
-        board[i+1][j]=1;
-        return 1;
-      }
-    }
-  }
 
-  return 0;
-}
-int dfs_left(int i,int j,int n) {
-  if (i<=0 || i>h || j<=0 || j>w) return 0; //出界
-  if (board[i][j]==1) { //撞墙
-    board[i][j]=0;
-    return -1;
-  }
-  if (board[i][j]==3) {
-    if (n<res || res==0) res=n;
-    return 1; //到达终点
-  }
-  //继续向上移动
-  int stat=dfs_left(i,j-1,n);
-  if (stat==1) return 1;
-  if (stat==0) return 0;
-  if (stat==-1) {
-    if (i-1>0 && board[i-1][j]!=1) {
-      if (dfs_up(i-1,j,n+1)) {
-        board[i][j-1]=1;
-        return 1;
-      }
-    }
-    if (i+1<=h && board[i+1][j]!=1) {
-      if (dfs_down(i+1,j,n+1)) {
-        board[i][j-1]=1;
-        return 1;
-      }
-    }
-    if (j-1>0 && board[i][j-1]!=1) {
-      if (dfs_left(i,j-1,n+1)) {
-        board[i][j-1]=1;
-        return 1;
-      }
-    }
-    if (j+1<=w && board[i][j+1]!=1) {
-      if (dfs_right(i,j+1,n+1)) {
-        board[i][j-1]=1;
-        return 1;
-      }
-    }
-  }
-
-  return 0;
-}
-int dfs_right(int i,int j,int n) {
-  if (i<=0 || i>h || j<=0 || j>w) return 0; //出界
-  if (board[i][j]==1) { //撞墙
-    board[i][j]=0;
-    return -1;
-  }
-  if (board[i][j]==3) {
-    if (n<res || res==0) res=n;
-    return 1; //到达终点
-  }
-  //继续向上移动
-  int stat=dfs_right(i,j+1,n);
-  if (stat==1) return 1;
-  if (stat==0) return 0;
-  if (stat==-1) {
-    if (i-1>0 && board[i-1][j]!=1) {
-      if (dfs_up(i-1,j,n+1)) {
-        board[i][j+1]=1;
-        return 1;
-      }
-    }
-    if (i+1<=h && board[i+1][j]!=1) {
-      if (dfs_down(i+1,j,n+1)) {
-        board[i][j+1]=1;
-        return 1;
-      }
-    }
-    if (j-1>0 && board[i][j-1]!=1) {
-      if (dfs_left(i,j-1,n+1)) {
-        board[i][j+1]=1;
-        return 1;
-      }
-    }
-    if (j+1<=w && board[i][j+1]!=1) {
-      if (dfs_right(i,j+1,n+1)) {
-        board[i][j+1]=1;
-        return 1;
-      }
-    }
-  }
-
-  return 0;
-}
 int main() {
   while (cin>>w>>h) {
-    res=0;
     if (w==0 && h==0) break;
+    res=INF; steps=1;
+    memset(board,0,sizeof(board)); //不对数组初始化必定WA，什么原理？
     for (int i=1;i<=h;i++) {
       for (int j=1;j<=w;j++) {
         cin>>board[i][j];
+        if (board[i][j]==2) {si=i;sj=j;}
+        if (board[i][j]==3) {gi=i;gj=j;}
       }
     }
 
-    for (int i=1;i<=h;i++) {
-      for (int j=1;j<=w;j++) {
-        if (board[i][j]==2) {
-          if (i-1>0 && board[i-1][j]!=1) dfs_up(i-1,j,1);
-          if (i+1<=h && board[i+1][j]!=1) dfs_down(i+1,j,1);
-          if (j-1>0 && board[i][j-1]!=1) dfs_left(i,j-1,1);
-          if (j+1<=w && board[i][j+1]!=1) dfs_right(i,j+1,1);
-          break;
-          }
-        }
-      }
-
-      if (res!=0 && res<=10) cout<<res<<endl;
-      else cout<<-1<<endl;
+    dfs(si,sj);
+    if (res>10) cout<<-1<<endl;
+    else cout<<res<<endl;
   }
+
   return 0;
 }
-
-
-/*
-出错数据
-10 10
-1 1 1 1 1 1 1 1 1 1
-0 1 1 1 1 1 1 0 1 1
-2 1 0 1 1 1 1 1 1 1
-1 1 1 1 1 1 0 1 1 1
-1 0 1 0 1 1 1 1 1 1
-1 1 1 1 1 3 1 0 1 1
-1 1 1 1 1 1 1 1 1 1
-1 1 1 1 1 1 1 1 0 1
-1 0 1 1 1 1 1 1 1 1
-1 1 1 1 1 1 1 1 1 1
-
-expected:9
-mine:-1
-*/
